6c97d8f729200b9af0c4d756353a9d3069566fb0
StylishPhoenix <an007567@gmail.com>
2023-09-16 11:16:01 -0500

    fixing stuff

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const { Client, GatewayIntentBits, PermissionFlagsBits, Permission, Events, Slas
 const { createCanvas, loadImage } = require('canvas');
 const {token} = require('./config.json')
 
+const fs = require('fs');
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ] });
 
-const kingdoms = ['Kingdom1', 'Kingdom2', 'Kingdom3', 'Kingdom4'];
-const kingdomControl = [30, 20, 25, 25];
 const currentKingdomControl = new SlashCommandBuilder()
     .setName('current_kingdom_control')
     .setDescription('Shows current kingdom control');
@@ -18,6 +18,8 @@ client.once('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
     await client.application.commands.set(commands);
     console.log('Commands registered.')
+    load_points();
+    save_points();
 });
 
 client.on('interactionCreate', async interaction => {
@@ -31,7 +33,7 @@ client.on('interactionCreate', async interaction => {
 
           kingdomControl.forEach((control, index) => {
             context.fillStyle = colors[index];
-            const controlAngle = (control / 100) * 2 * Math.PI;
+            const controlAngle = (control / 10000) * 2 * Math.PI;
             context.beginPath();
             context.moveTo(250, 250);
             context.arc(250, 250, 250, startAngle, startAngle + controlAngle);
@@ -44,5 +46,30 @@ client.on('interactionCreate', async interaction => {
         interaction.reply({ files: [attachment] });
     }
 });
+
+function save_points(){
+    let data = '';
+    for (const [kingdom, points] of Object.entries(kingdom_points)){
+        data += '${kingdom}:${points}\n';
+    }
+    fs.writeFileSync('kingdom_points.txt', data);
+}
+
+function load_points() {
+    kingdom_points = {};
+    kingdom_points.forEach(({ kingdom }) => {
+      kingdom_points[kingdom] = 0;
+    });
+  
+    if (fs.existsSync('kingdom_points.txt')) {
+      const lines = fs.readFileSync('kingdom_points.txt', 'utf-8').split('\n');
+      for (const line of lines) {
+        const [kingdom, points] = line.split(':');
+        if (kingdom && points && kingdom_points.hasOwnProperty(kingdom)) {
+          kingdom_points[kingdom] = parseInt(points, 10);
+        }
+      }
+    }
+  }
 
 client.login(token);
